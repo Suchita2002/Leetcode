cb8d2e11341c00fc1fc907e3b367e2e1e4949761
Suchita2002 <suchitajagtap522002@gmail.com>
2026-05-31 10:13:48 +0530

    Time: 25 ms (93.30%) | Memory: 107.5 MB (30.95%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution 
+{
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) 
+    {
+        Arrays.sort(asteroids);
+
+        long m = mass;
+
+        for (int a : asteroids) 
+        {
+            if (m < a) 
+            {
+                return false;
+            }
+            m += a;
+        }
+
+        return true;
+    }
+}
